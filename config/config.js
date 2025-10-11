57c6affb344833406d75ffcf2fe4272bf54465c9
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 <syed.anwar@golootlo.pk>
2025-10-11 14:37:59 +0500

    basic setup done

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ import {config as conf} from "dotenv";
 conf();
 const _config = {
     port: process.env.PORT,
-    dbURl: process.env.MONGO_DB_URI
-
+    dbURl: process.env.MONGO_DB_URI,
+    env: process.env.ENV
 }
 
 export const config = Object.freeze(_config);
--- serverMain.js
+++ serverMain.js
@@ -1,13 +1,25 @@
 import app from './src/main.js'
 import { config } from './config/config.js';
 import mongoose from "mongoose";
-
+import userRoutes from "./routes/userRoutes.js";
 const port = config.port;
 
+// Define route BEFORE listen
+app.use('/users', userRoutes)
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.log("aaya");
+  res.status(statusCode).json({
+    message: err.message,
+    errorStack: config.env === "development" ? err.stack : ""
+  });
+});
+
+// Start server and connect DB
 app.listen(port, () => {
-        mongoose.connect(config.dbURl);
-        mongoose.connection.on('connected', () => {
-            console.log('db connected')
-        });
-    console.log(`server is running on ${port} now`);
-})
+  mongoose.connect(config.dbURl);
+  mongoose.connection.on('connected', () => console.log('db connected'));
+  console.log(`Server is running on port ${port}`);
+});
